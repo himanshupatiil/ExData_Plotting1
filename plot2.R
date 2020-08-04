8c1609bde164ba4fa56bcc5c769473aae339393a
himanshupatiil <68125008+himanshupatiil@users.noreply.github.com>
2020-08-04 17:23:27 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -8,10 +8,12 @@ subdata$Time <- strptime(subdata$Time, format="%H:%M:%S")
 subdata[1:1440,"Time"] <- format(subdata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 subdata[1441:2880,"Time"] <- format(subdata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
+#plotting in png
+png("plot2.png", width=480, height=480)
+
 
 # calling the basic plot function
 plot(subdata$Time,as.numeric(as.character(subdata$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 
-#plotting in png
-png("plot2.png", width=480, height=480)
+
 dev.off()
