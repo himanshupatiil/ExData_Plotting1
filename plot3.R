5765a4669e158a5bc538f7f8d293347b99ddc26c
himanshupatiil <68125008+himanshupatiil@users.noreply.github.com>
2020-08-04 17:24:23 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,6 +7,10 @@ subdata$Date <- as.Date(subdata$Date, format="%d/%m/%Y")
 subdata$Time <- strptime(subdata$Time, format="%H:%M:%S")
 subdata[1:1440,"Time"] <- format(subdata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 subdata[1441:2880,"Time"] <- format(subdata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+#plotting in png
+png("plot3.png", width=480, height=480)
+
 #calling the basic plot functions
 plot(subdata$Time,subdata$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
 with(subdata,lines(Time,as.numeric(as.character(Sub_metering_1))))
@@ -14,6 +18,5 @@ with(subdata,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
 with(subdata,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-#plotting in png
-png("plot3.png", width=480, height=480)
+
 dev.off()
